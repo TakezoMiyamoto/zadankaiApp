904e547af6cbaa6f79ef75ebd655e630264d4422
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-04-08 17:36:52 +0900

    #11 add class name in relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :joiner_user
-  belongs_to :joined_project
+  belongs_to :joiner_user, class_name: "User"
+  belongs_to :joined_project, class_name: "Project"
+
+  
 end

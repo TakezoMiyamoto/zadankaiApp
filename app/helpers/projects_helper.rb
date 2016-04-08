3639a2bdebac43f780ec3134919ddeb8256899b2
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-04-08 10:16:17 +0900

    #6 add youtube link with project helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+  def embed(youtube_url)
+    if @project.youtube_url.present?
+        youtube_id = youtube_url.split("=").last
+        content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+    end
+  end
 end

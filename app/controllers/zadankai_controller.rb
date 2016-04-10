4daeac96ce59b19e44d38eba29e00b8c09c3ce56
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-04-10 12:53:52 +0900

    #29 add title and meta information.

--- app/controllers/zadankai_controller.rb
+++ app/controllers/zadankai_controller.rb
@@ -2,8 +2,14 @@ class ZadankaiController < ApplicationController
   def home
      # 直近のプロジェクト６件を取得
     @projects = Project.order(created_at: :desc).limit(6)
+    @title = "Home"
   end
 
   def about
+    @title = "Zadankaiとは？"
+  end
+
+  def readyfor
+    @title = "プロジェクトをはじめる"
   end
 end
